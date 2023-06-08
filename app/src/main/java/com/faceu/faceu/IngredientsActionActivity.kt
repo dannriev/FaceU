9068a70a959ee8b56376824982cd74beab4dfeee
rudisetiawan <rudistiawannn@gmail.com>
2023-06-08 17:56:06 +0800

    progress 4

---
+++ app/src/main/java/com/faceu/faceu/IngredientsActionActivity.kt
@@ -0,0 +1,11 @@
+package com.faceu.faceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngredientsActionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredients_action)
+    }
+}
