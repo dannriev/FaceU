b308708894ec895c3ed7e8655258ba78eeafaa67
rudisetiawan <rudistiawannn@gmail.com>
2023-06-08 14:39:46 +0800

    progress 2

---
+++ app/src/main/java/com/faceu/faceu/CreateMethodActivity.kt
@@ -0,0 +1,11 @@
+package com.faceu.faceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateMethodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_method)
+    }
+}
---
+++ app/src/main/java/com/faceu/faceu/FaceCameraActivity.kt
@@ -0,0 +1,11 @@
+package com.faceu.faceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FaceCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_face_camera)
+    }
+}
