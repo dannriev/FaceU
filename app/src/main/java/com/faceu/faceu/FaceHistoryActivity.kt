0ecaef79e847f3849f903e365eba2933898fd5c0
rudisetiawan <rudistiawannn@gmail.com>
2023-06-08 16:37:03 +0800

    progress 3

---
+++ app/src/main/java/com/faceu/faceu/FaceHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.faceu.faceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FaceHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_face_history)
+    }
+}
---
+++ app/src/main/java/com/faceu/faceu/IngredientsResultActivity.kt
@@ -0,0 +1,13 @@
+package com.faceu.faceu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngredientsResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredients_result)
+
+
+    }
+}
