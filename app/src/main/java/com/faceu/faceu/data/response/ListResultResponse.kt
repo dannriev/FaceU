107dd14aec7e39929bfcb38728aa518dedce0319
dann_ <89714929+dannriev@users.noreply.github.com>
2023-06-16 17:12:23 +0800

    del

--- app/src/main/java/com/faceu/faceu/data/response/ListResultResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.faceu.faceu.data.response
-
-import com.google.gson.annotations.SerializedName
-
-data class ListResultResponse(
-
-	@field:SerializedName("predictions")
-	val predictions: List<PredictionsItem>
-)
